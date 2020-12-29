1d67db3aea3989dd5816c614ac1f6952cdacfaf1
RJ Taylor <70663575+rjtayl@users.noreply.github.com>
2020-12-29 10:44:43 -0500

    Add weighting potential vs z plots to Plotting.jl

--- SSD/Plotting.jl
+++ SSD/Plotting.jl
@@ -101,3 +101,79 @@ function PlotCV(sims::AbstractVector{Simulation}, dir::String="", prefix::String
 
     savefig(dir * prefix * "deriv_cv" * suffix * extension)
 end
+
+function PlotWP(sim::Simulation, dir::String="", prefix::String="", suffix::String="")::Nothing
+    #Edge and corner plots only work for hexagons
+    @info "Plotting weighting potentials vs z"
+    
+    WP = sim.weighting_potentials[2]
+    grid = WP.grid
+    Lx, Ly, Lz = size(grid)
+    R = sim.detector.contacts[2].geometry.rOuter
+    r = sqrt(3)*R/2
+    edge = (r, 0.0)
+    corner = (r, R/2)
+
+    x0 = 0
+    y0 = 0
+    xe = 0
+    ye = 0
+    xc = 0
+    yc = 0
+
+
+    for i in 1:Lx
+        if grid[i,1,1][1] == 0
+           x0 = i
+        end
+    end
+
+
+    for i in 1:Ly
+        if grid[1,i,1][2] == 0
+           y0 = i
+        end
+    end
+
+    for i in 1:Lx
+        if grid[i,1,1][1] -edge[1] < 0.000001
+           xe = i
+        end
+    end
+    
+    ye = y0
+    
+    for i in 1:Lx
+        if grid[i,1,1][1] -corner[1] < 0.000001
+           xc = i
+        end
+    end
+    
+    for i in 1:Ly
+        if grid[1,i,1][2] -corner[2] < 0.000001
+           yc = i
+        end
+    end
+
+    is = 1:1:Lz
+    wp0 = convert(Array{T},similar(is))
+    wpe = convert(Array{T},similar(is))
+    wpc = convert(Array{T},similar(is))
+
+    for i in is
+        wp0[i] = WP[x0,y0,i]
+        wpe[i] = WP[xe,ye,i]
+        wpc[i] = WP[xc,yc,i]
+    end
+
+    zs = convert(Array{T},similar(is))
+
+    for i in eachindex(is)
+        zs[i] = grid[x0,y0,i][3]
+    end
+    
+    plot(plot(zs, wp0, title="Center"), plot(zs, wpe, title="Edge"), plot(zs, wpc, title="Corner"), layout=(1, 3), size=figSize)
+    savefig(dir * prefix * "weighting_potential_vs_z" * suffix * extension)
+end
+  
+    
