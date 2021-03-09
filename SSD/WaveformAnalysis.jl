d80b919b679c0f16b4d55bc4d5bb922633066760
RJ Taylor <70663575+rjtayl@users.noreply.github.com>
2021-03-09 11:06:42 -0500

    Add CollectRiseTimesFiltered to WaveformAnalysis

--- SSD/WaveformAnalysis.jl
+++ SSD/WaveformAnalysis.jl
@@ -31,3 +31,11 @@ function CollectRiseTimes(events, low = 0.1, high = 0.9)
     end
     return riseTimes
 end
+
+function CollectRiseTimesFiltered(events, low = 0.1, high = 0.9; dt = 1e-9)
+    riseTimes = Vector{Unitful.Time}()
+    for i in 1:length(events)
+        push!(riseTimes, CalculateRiseTime(events[i].waveforms[1].time, CustomFilter(events[i].waveforms[1].value, dt), low, high))
+    end
+    return riseTimes
+end
