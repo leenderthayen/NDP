404fa495fb7986f13d1e17987b84d9ad7128d5ff
RJ Taylor <70663575+rjtayl@users.noreply.github.com>
2021-06-14 15:44:04 -0400

    Minor MainScript.jl fixes

--- SSD/MainScript.jl
+++ SSD/MainScript.jl
@@ -10,27 +10,34 @@ include("src/DigitalFilters.jl")
 @info "Start"
 
 # Setup the simulation
-simFilename = "save/hex_pixel_Si_ring_circle.h5"
-geometryFile = "config_files/simple_si_pixel_ring_circle.json"
-chargeDriftConfigFile = "config_files/drift_velocity_config_linear_si.json"
-max_ref = 3
+simFilename = "save/1Hex_simple.h5"
+geometryFile = "config/geometry/Si 1Hexagon 2.0-10.3-0.1.json"
+chargeDriftConfigFile = "config/mobility/drift_velocity_si_140K.json"
+CCDFile = "config/ccd/1Hex_simple.jl"
+include(CCDFile)
+max_ref = 2
 
 dataDirectory = "../analysis/Power_Spectrum/"
-noiseFilename = dataDirectory+"Run74_0.root"
+noiseFilename = string(dataDirectory, "Run74_0.root")
 
+@info "Loading Simulation"
 # Get either a saved simulation or a new one
 sim = ReadSimulation(simFilename)
 if isnothing(sim)
-    sim = SetupDefaultSimulation(geometryFile)
+    sim = SetupDefaultSimulation(geometryFile, CCDFile)
+    sim.detector.contacts[1].potential = -150
     CalculateDefaultDetectorFields!(sim, chargeDriftConfigFile, max_ref)
     SaveSimulation(simFilename, sim)
 end
 
+@info "Setting CDM"
 SetChargeDriftModel!(sim, chargeDriftConfigFile)
 
-geantFilename = "../data/e_500keV.root"
+@info "Loading Geant Data"
+geantFilename = "../data/e-_60keV_0inc.root"
 gdf = GetHitInformation(geantFilename)
 
+@info "Applying Noise"
 # [Noise Filename , Scalar for the noise amplitude]: Noise has a fixed amplitude, but the SSD waveforms are scaled to be 1.
 # Roughly taking 60 keV = 40 ADC channel | 1 = E_sim, scalar needs to be 60 / (40 * E_sim)
 elecNoise = GetNoiseROOT(noiseFilename,0.003)
